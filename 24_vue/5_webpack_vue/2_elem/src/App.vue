83b26b0625e7fc081cbb7ce68b4c3de792d1e68d
tecWang <tecWang@users.noreply.github.com>
2018-01-05 23:05:24 +0800

    主页样式重置；添加Header组件

--- 24_vue/5_webpack_vue/2_elem/src/App.vue
+++ 24_vue/5_webpack_vue/2_elem/src/App.vue
@@ -1,33 +1,48 @@
 <template>
   <div id="app">
-    <v-star></v-star>
-    <v-split></v-split>
+    <v-header :seller="seller"></v-header>
+
     <v-shopcart></v-shopcart>
   </div>
 </template>
 
 <script>
 import data from './common/json/data.json';
-import star from './components/star/star.vue';
-import split from './components/split/split.vue';
+import Header from './components/header/header.vue'
 import shopcart from './components/shopcart/shopcart.vue';
+
+import {urlParse} from './common/js/util';
 
 export default {
   name: 'app',
   components: {
-    'v-star': star,
-    'v-split': split,
+    'v-header': Header,
     'v-shopcart': shopcart
+  },
+  data() {
+    return {
+      seller: {},
+      id: (() => {
+        let queryParam = urlParse();
+        console.log(queryParam);
+        return queryParam.id;
+      })()
+    };
+  },
+  created() {
+//      this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
+//        response = response.body;
+//        if (response.errno === ERR_OK) {
+//          this.seller = response.data;
+//          this.seller = Object.assign({}, this.seller, response.data);
+//        }
+//      });
+
+    this.seller = data.seller;
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl";
 </style>
