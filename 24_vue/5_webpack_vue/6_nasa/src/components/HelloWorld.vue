b0ca50f5354c90734d8723d52db5a1a2f3c437aa
tecWang <tecWang@users.noreply.github.com>
2018-02-07 10:51:47 +0800

    NASA数据共享

---
+++ 24_vue/5_webpack_vue/6_nasa/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<div>{{title}}</div>
+		<img src="imageUrl">
+		<div>{{explanation}}</div>
+	</div>
+</template>
+
+<script>
+import "vue-resource"
+
+console.log(this.$http);
+
+export default {
+  	name: 'HelloWorld',
+  	data () {
+		return {
+			title: 'default',
+			imageUrl: 'deafult',
+			explanation: 'default'
+		}
+  	},
+	created(){
+	  	this.getDailyInfo();
+	  	console.log("reday");
+	},	
+	methods:{
+		getDailyInfo(){
+			console.log(this.$http);
+			this.$http.get('https://api.nasa.gov/planetary/apod?api_key=XK69v1f4Xfl0xGtE7SCEpYuw5zYPcNTzkI2AI23q')
+				.then(function(res){
+					console.log(res);
+					this.title = res.title;
+					this.imageUrl = res.url;
+					this.explanation = res.explanation;
+				});
+		}
+	}
+}
+</script>
+
